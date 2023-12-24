0c5cecb3554567ee06a632982bf2e69d10cb0c7f
sidhant dhyani <siddhantydhyani99@gmail.com>
2023-12-24 19:09:40 +0530

    admin auth opened complete

--- models/admin.js
+++ models/admin.js
@@ -1,16 +1,16 @@
-// const mongoose = require("mongoose");
-// const AdminSchema = new mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-// });
+const mongoose = require("mongoose");
+const AdminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
 
-// const AdminModel = mongoose.model('Admin', AdminSchema);
-// module.exports = AdminModel;
+const AdminModel = mongoose.model('Admin', AdminSchema);
+module.exports = AdminModel;
